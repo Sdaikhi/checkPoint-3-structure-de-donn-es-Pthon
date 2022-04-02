874147f1c89e681c83bb686df58acb8c2f1da512
Sdaikhi <101455784+Sdaikhi@users.noreply.github.com>
2022-04-02 12:04:34 +0200

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+#exercice 3
+
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+d3 = {'a': 300+100, 'b': 200+200, 'd':400, 'c':300}
+print(d3)
+
+
+# In[16]:
+
+
+#exercice 1
+my_list = [2, 3, 6]
+p = 1
+for i in my_list :
+    p = p*i
+    
+print(p)
+
+
+# In[30]:
+
+
+#exercice 2
+list1= [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+list1.sort(reverse=True)
+list1
+
+
+# In[29]:
+
+
+#exercice 5     
+    tup.sort(key = lambda x: float(x[1]), reverse = True) 
+    print(tup) 
+  
+list = [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]
+print(tup)
+
+
+# In[ ]:
+
+
+#exercice 4
+jsp
+
